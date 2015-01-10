f97b490c7630634a394d1aa96bd7c6bc54e7c655
Adrian Claudiu Covaci <adrianclaudiucovaci@gmail.com>
2015-01-10 18:48:10 +0200

    Upload existing files

---
+++ wp-content/themes/blank/header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Tutorial theme</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+		<div class="navwrapper">
+			<div class="logowrapper"></div>
+			<nav>
+				<ul class="nav">
+					<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+				</ul>
+			</nav>
+		</div>
+<body>
+<div id="wrapper">
+<div id="header">
+<h1>HEADER</h1>
+</div>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
